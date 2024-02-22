f80ddec97a72f68f61d7c2ec623ae558b3dff897
Oltion-Koceku <koceku17@gmail.com>
2024-02-22 15:48:00 +0100

    [fix]fizzbuzz

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 const container = document.querySelector(".container-ol")
 console.log(container);
 for(let i = 1; i <= 100; i++){
-  if(i % 3 === 0){
-    container.innerHTML +=`
-    <div class="box">
-      ${"fizz"}
-    </div>
-    `; 
-    console.log("fizz");
-  }else if(i % 5 === 0){
-    container.innerHTML +=`
-    <div class="box">
-      ${"buzz"}
-    </div>
-    `
-    console.log("buzz");
-  }else if ((i % 3 === 0) && (i % 5 === 0)){
+  if((i % 3 === 0) && (i % 5 === 0)){
     container.innerHTML +=`
     <div class="box">
       ${"fizzbuzz"}
     </div>
+    `;
+  }else if(i % 5 === 0){
+    container.innerHTML +=`
+    <div class="box">
+    ${"buzz"}
+    </div>
     `
+    console.log("buzz");
+  }else if (i % 3 === 0){
+    container.innerHTML +=`
+    <div class="box">
+      ${"fizz"}
+    </div>
+    `;
+    console.log("fizz");
   }else{
     container.innerHTML +=`
     <div class="box">
