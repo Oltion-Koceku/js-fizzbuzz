beeea0c1dd23096fee5047a55e93e4a4028ae994
Oltion-Koceku <koceku17@gmail.com>
2024-02-22 16:00:43 +0100

    [end] fine esercizio

--- js/script.js
+++ js/script.js
@@ -3,27 +3,27 @@ console.log(container);
 for(let i = 1; i <= 100; i++){
   if((i % 3 === 0) && (i % 5 === 0)){
     container.innerHTML +=`
-    <div class="box">
-      ${"fizzbuzz"}
+    <div class="box fizzbuzz">
+      ${"FizzBuzz"}
     </div>
     `;
   }else if(i % 5 === 0){
     container.innerHTML +=`
-    <div class="box">
-    ${"buzz"}
+    <div class="box buzz">
+    ${"Buzz"}
     </div>
     `
-    console.log("buzz");
+    console.log("Buzz");
   }else if (i % 3 === 0){
     container.innerHTML +=`
-    <div class="box">
-      ${"fizz"}
+    <div class="box fizz">
+      ${"Fizz"}
     </div>
     `;
-    console.log("fizz");
+    console.log("Fizz");
   }else{
     container.innerHTML +=`
-    <div class="box">
+    <div class="box numeri">
       ${i}
     </div>
     `;
